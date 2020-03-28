b25e0c87d26a3a5f7c61c663a949e1703723a92d
carlosfusco <carlos_fusco@hotmail.com>
2020-03-28 18:18:30 +0000

    Create an Indicator Badge Lightning Component step.

--- Build Reusable Lightning Components/force-app/main/default/aura/IndicatorBadges/IndicatorBadgesController.js
+++ Build Reusable Lightning Components/force-app/main/default/aura/IndicatorBadges/IndicatorBadgesController.js
@@ -1,8 +1,10 @@
 ({
   doInit: function(component, event, helper) {
+    var recId = component.get("v.recordId");
     var sObj = component.get("v.sObjectName");
     if (sObj) {
       helper.getLabelForRecord(component, sObj);
+      helper.getBadgesForRecord(component, recId, sObj);
     }
   }
   //future code here
